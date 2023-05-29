e640740936387f5f74aea03c3de6cd267e3ee8ca
yurnero14 <100948221+yurnero14@users.noreply.github.com>
2023-05-30 00:57:27 +0200

    LAb 1 changes

--- Lab1/ex1.py
+++ Lab1/ex1.py
@@ -2,6 +2,10 @@ import sys
 
 def compute_Avg_scores(lscores):
     return sum(sorted(lscores)[1:-1])
+    """
+    In the function above, as per the first requirement the highest and lowest score is removed but first it is sorted
+    in ascending order
+    """
 
 class Competitor:
     def __init__(self, name, surname, country, scores):
@@ -11,10 +15,16 @@ class Competitor:
         self.scores = scores
         self.avg_score = compute_Avg_scores(scores)
 
+"""
+A class is created since we have different attributes for the competitor and we need an object to store the information
+regarding the competitor 
+"""
+
 if __name__ == '__main__':
-    l_bestCompetitors = []
-    hCountryScores ={}
+    l_bestCompetitors = []  # we are storing the best competitors in a list
+    hCountryScores ={}  # instantiating a dictionary
     with open(sys.argv[1]) as f:
+        # here we are specifying that we will take the file from command line's second argument [1]
         for line in f:
             name, surname, country = line.split()[0:3]
             scores = line.split()[3:]
@@ -29,10 +39,14 @@ if __name__ == '__main__':
             l_bestCompetitors.append(comp)
 
             if len(l_bestCompetitors) >= 4:
+                # we need three best competitors so it makes sense that we start sorting them on their scores
                 l_bestCompetitors = sorted(l_bestCompetitors, key=lambda i: i.avg_score)[::-1][0:3]
+                # [::-1] means to traverse the iterable in reverse since sorted gives ascending order and then we sliced
+
             if comp.country not in hCountryScores:
                 hCountryScores[comp.country] = 0
             hCountryScores[comp.country] += comp.avg_score
+            # using country as key we are populating the dictionary. this is second requirement of excercise 1
 
     if len(hCountryScores) == 0:
         print('No competitors')
@@ -40,6 +54,7 @@ if __name__ == '__main__':
 
     best_country = None
     for count in hCountryScores:
+        # count is the dummy variable for the key of the dictionary and its value is then being compared
         if best_country is None or hCountryScores[count] > hCountryScores[best_country]:
             best_country = count
 
@@ -50,4 +65,7 @@ if __name__ == '__main__':
     print('Best Country:')
     print("%s - Total score: %.1f" % (best_country, hCountryScores[best_country]))
 
-
+    """
+    The final part of the code is C-style formatting which looks like a pain in the ass. there are different 
+    and easier ways for me to do it 
+    """
